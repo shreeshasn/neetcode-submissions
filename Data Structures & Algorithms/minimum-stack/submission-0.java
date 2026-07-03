7b1bb661f85ee8bb3b0363e795a149d8f5fe25ea
Shreesha S Nekkar <shreeshasnekkar81@gmail.com>
2026-07-03 19:06:57 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.java
@@ -0,0 +1,38 @@
+class MinStack {
+
+    List<Integer> stack = new ArrayList<>();
+    int top = -1;
+    // int getmin = Math.pow(2,31)-1;
+
+    public MinStack() {
+        
+    }
+    
+    public void push(int val) {
+        stack.add(val);
+        // getmin = Math.min(getmin , val);
+        top = top + 1;
+    }
+    
+    public void pop() {
+        stack.remove(top);
+        // for(int i : stack)
+        // {
+        //     getmin = Math.min(i,getmin)
+        // }
+        top = top - 1;
+    }
+    
+    public int top() {
+        return stack.get(top);
+    }
+    
+    public int getMin() {
+        int getmin = stack.get(top);
+        for(int i : stack)
+        {
+            getmin = Math.min(getmin , i);
+        }
+        return getmin;
+    }
+}
